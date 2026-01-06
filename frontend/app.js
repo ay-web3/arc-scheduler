d74b0b29d4c4a7a9f8d173175f3a2cc5c21196c5
ay-web3 <ayomideemmanuel3002@gmail.com>
2026-01-06 20:06:49 +0000

    refactor: explicitly bind wallet UI elements

--- frontend/app.js
+++ frontend/app.js
@@ -57,7 +57,7 @@ async function connectWallet() {
               nativeCurrency: {
                 name: "USDC",
                 symbol: "USDC",
-                decimals: 6,
+                decimals: 18,
               },
               rpcUrls: ["https://rpc.testnet.arc.network"],
               blockExplorerUrls: ["https://testnet.arcscan.app/"],
